f61669456ec434986fb1cabb057c684a92a37a59
mortein43 <mortein43@gmail.comm>
2023-10-20 17:39:20 +0300

    Добавьте файлы проекта.

---
+++ 0301.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+void generateSound(double frequency, int duration) {
+	Beep(frequency, duration);
+}
+
+int main()
+{
+	cout << "C";
+	generateSound(261.63, 300);//До
+	generateSound(293.66, 300);//Ре
+	generateSound(329.63, 300);//Ми
+	generateSound(349.23, 300);//Фа
+	generateSound(392, 300);//Соль
+	generateSound(440, 300);//Ля
+	generateSound(493.88, 300);//Си
+	generateSound(523.25, 500);//До ІІ
+	generateSound(523.25, 300);//До ІІ
+	generateSound(493.88, 300);//Си
+	generateSound(440, 300);//Ля
+	generateSound(392, 300);//Соль
+	generateSound(349.23, 300);//Фа
+	generateSound(329.63, 300);//Ми
+	generateSound(293.66, 300);//Ре
+	generateSound(261.63, 600);//До
+
+	return 0;
+}
